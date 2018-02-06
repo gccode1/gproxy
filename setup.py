081833786c910b274a267bc03f69055683c24155
Gaurav Singh <gaurav.s@minoty.com>
2018-02-06 17:35:57 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
+from os import path
 
-try:
-    with open('README.md') as f:
-        long_description = f.read()
-except IOError:
-    long_description = ''
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='gproxy',
-    version=open('VERSION').read(),
-    description='requests Session based proxy in Python.',
+    version=open('VERSION').read().strip(),
+    description='Session based proxy in Python.',
     author='Gaurav Singh',
     author_email='gauravsingh042@gmail.com',
     url='https://github.com/gccode1/gproxy',
     packages=['gproxy'],
+    license='',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
@@ -27,7 +29,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    license='GNU LGPL v2.1',
+    keywords='gproxy proxy https http',
+    py_modules=['gproxy'],
     long_description=long_description,
-    requires=['requests', "bs4"],
+    install_requires=['requests', "bs4"],
 )
